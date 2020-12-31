7cbddd87581f5022aca4fab0603c522bd97ec355
Gabriel Vieira <gabrielvie@users.noreply.github.com>
2020-12-31 01:18:10 -0300

    refactor(transaction): :zap: to improve better UX user's transactions was moved to main.dart and then converted main.dart to statefull widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
+import 'package:expenses_app/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
-import 'package:expenses_app/widgets/user_transaction.dart';
+import 'package:expenses_app/models/transaction.dart';
+import 'package:expenses_app/widgets/transaction_form.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String title = 'Flutter App';
+  final String title = 'Personal Expenses';
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: this.title,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: this.title),
@@ -22,16 +25,63 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   final String title;
 
   MyHomePage({@required this.title});
 
   @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      uuid: '5027aec8-33bc-48bf-809d-7f6117fcf682',
+      title: 'New Shoes',
+      amount: 69.99,
+      dateTime: DateTime.now(),
+    ),
+    Transaction(
+      uuid: 'f93d587f-d1f9-4591-a727-988f0dc9f05c',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      dateTime: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String transactionTitle, double transactionAmound) {
+    var uuid = Uuid();
+    final newTransaction = Transaction(
+      uuid: uuid.v4(),
+      title: transactionTitle,
+      amount: transactionAmound,
+      dateTime: DateTime.now(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTransaction);
+    });
+  }
+
+  void _showTransactionForm(BuildContext buildContext) {
+    showModalBottomSheet(
+      context: buildContext,
+      builder: (_) {
+        return GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          child: TransactionForm(_addNewTransaction),
+          onTap: () {},
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -45,9 +95,13 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransactions()
+            TransactionList(_userTransactions)
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _showTransactionForm(context),
       ),
     );
   }
--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -1,54 +1,20 @@
 import 'package:flutter/material.dart';
 
-import 'package:expenses_app/models/transaction.dart';
-import 'package:expenses_app/widgets/transaction_form.dart';
-import 'package:expenses_app/widgets/transaction_list.dart';
-import 'package:uuid/uuid.dart';
-
 class UserTransactions extends StatefulWidget {
   @override
   _UserTransactionsState createState() => _UserTransactionsState();
 }
 
 class _UserTransactionsState extends State<UserTransactions> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-      uuid: '5027aec8-33bc-48bf-809d-7f6117fcf682',
-      title: 'New Shoes',
-      amount: 69.99,
-      dateTime: DateTime.now(),
-    ),
-    Transaction(
-      uuid: 'f93d587f-d1f9-4591-a727-988f0dc9f05c',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      dateTime: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: <Widget>[
-          TransactionForm(_addNewTransaction),
-          TransactionList(_userTransactions),
+          // TransactionForm(_addNewTransaction),
+          // TransactionList(_userTransactions),
         ],
       ),
     );
-  }
-
-  void _addNewTransaction(String transactionTitle, double transactionAmound) {
-    var uuid = Uuid();
-    final newTransaction = Transaction(
-      uuid: uuid.v4(),
-      title: transactionTitle,
-      amount: transactionAmound,
-      dateTime: DateTime.now(),
-    );
-
-    setState(() {
-      _userTransactions.add(newTransaction);
-    });
   }
 }

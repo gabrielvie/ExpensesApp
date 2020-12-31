7ba0335010efaabb34c8e2c883ae4723f1836283
Gabriel Vieira <gabrielvie@users.noreply.github.com>
2020-12-31 17:21:03 -0300

    feat(transaction): :sparkles: ChartBar widget.

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPctOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPctOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        FittedBox(child: Text('\$' + spendingAmount.toStringAsFixed(2))),
+        SizedBox(height: 4),
+        Container(
+          height: 100,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1,
+                  ),
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: spendingPctOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 4),
+        Text(label),
+      ],
+    );
+  }
+}

b7d009d381f28d742ce7298dec643450b02a032c
Gabriel Vieira <gabrielvie@users.noreply.github.com>
2020-12-31 13:24:51 -0300

    feat(transaction): :lipstick: added an image that shows when the app doesn't have a transaction stored.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,12 +12,29 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return TransactionCard(transactions[index]);
-        },
-        itemCount: transactions.length,
-      ),
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                Text(
+                  'There is no transactions',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(height: 20),
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/crab.png',
+                    fit: BoxFit.cover,
+                  ),
+                )
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return TransactionCard(transactions[index]);
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
